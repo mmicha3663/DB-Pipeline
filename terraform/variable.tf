8b3087a244ca3331b91cd48823a7e40a7e2b8bc7
mmicha3663 <85208339+mmicha3663@users.noreply.github.com>
2023-11-07 20:39:35 -0600

    Update variable.tf

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -1,17 +1,17 @@
 variable "db_password" {
   description = "Password for Redshift master DB user"
   type        = string
-  default     = ""
+  default     = "Meowmix123"
 }
 
 variable "s3_bucket" {
   description = "Bucket name for S3"
   type        = string
-  default     = ""
+  default     = "MatthewMichael_reddit_bucket"
 }
 
 variable "aws_region" {
   description = "Region for AWS"
   type        = string
-  default     = ""
+  default     = "us-east-2"
 }
